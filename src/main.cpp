b538b45ede811f560048d556c7c1b2cd5dbe6abb
2spooky555 <rdeivert@icloud.com>
2023-10-10 14:54:30 -0700

    session 7 updated

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../external/stb_image.h"
 //#include "../external/glm/glm.hpp"
 //#include "../external/glm/gtc/matrix_transform.hpp"
 #include "../external/glm/gtc/type_ptr.hpp"
